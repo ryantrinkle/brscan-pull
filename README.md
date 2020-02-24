4c9c8ed21c8cc7f81bfc0e4c98ac78478721f71f
Ryan Trinkle <ryan@trinkle.org>
2020-02-24 17:57:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# brscan-pull: pull-based scanning for Brother devices
+
+## Usage
+
+```bash
+brscan-pull FRIENDLY-NAME
+```
+
+Where `FRIENDLY-NAME` is a printer that was configured by a command like `brsaneconfig4 -a name=FRIENDLY-NAME model=MODEL-NAME ip=xx.xx.xx.xx` or a NixOS configuration like:
+
+```nix
+  hardware.sane = {
+    enable = true;
+    brscan4 = {
+      enable = true;
+      netDevices = {
+        home = {
+          ip = "1.2.3.4";
+          model = "MFC-L1234CDW";
+        };
+      };
+    };
+  };
+```
+
+## Installation
+
+Clone this repo:
+
+```bash
+cd /etc/nixos
+sudo git clone https://github.com/ryantrinkle/brscan-pull
+```
+
+Add something like this to your NixOS configuration:
+
+```nix
+  environment.systemPackages = [
+    (pkgs.callPackage ./brscan-pull {})
+  ];
+```
+
+You can also install brscan-pull using `home-manager` or `nix-env -i`.
+
+## Configuration
+
+If you want to pass additional arguments to `scanimage` when you scan, you can do so by adding an argument to the invocation of brscan-pull, like so:
+
+```nix
+  environment.systemPackages = [
+    (pkgs.callPackage ./brscan-pull { extraScanimageOptions = "--source 'Automatic Document Feeder(centrally aligned,Duplex)'"; })
+  ];
+```
+
+(It is not currently possible to pass additional arguments at runtime; this would be a welcome improvement!)
---
+++ default.nix
@@ -0,0 +1,39 @@
+{ writeScriptBin, bash, coreutils, sane-backends, tesseract4, evince, gnused
+, extraScanimageOptions ? "" # Extra options to provide to scanimage, e.g.: "--source 'Automatic Document Feeder(centrally aligned,Duplex)'"
+}:
+
+writeScriptBin "brscan-pull" ''
+  #!${bash}/bin/bash
+  set -euo pipefail
+  PATH=${coreutils}/bin
+
+  # Enumerating printers with scanimage -L is too slow (~12 seconds on my
+  # machine), so instead we look in the brother config files and try to guess
+  # the scanner's device name.  I'm not sure if this is really legitimate, but
+  # it seems like they just used the zero-based line number where the printer is
+  # found.
+
+  BROTHER_LINE_NUMBER="$(cat /etc/opt/brother/scanner/brscan4/brsanenetdevice4.cfg | ${gnused}/bin/sed -n "/^DEVICE=$1 ,/=")"
+  DEVICE_NUMBER=$(( $BROTHER_LINE_NUMBER - 1 ))
+  SANE_DEVICE_NAME="brother4:net1;dev$DEVICE_NUMBER"
+
+  PREFIX="$HOME/Documents/Scans"
+
+  TMPDIR="$(mktemp -d 2>/dev/null)"
+  trap "rm -rf \"$TMPDIR\"" EXIT
+
+  cd "$TMPDIR"
+
+  ${sane-backends}/bin/scanimage -d "$SANE_DEVICE_NAME" --batch="page%d.png" --format=png ${extraScanimageOptions}
+
+  ls page*.png | ${tesseract4}/bin/tesseract - out pdf
+
+  ${evince}/bin/evince out.pdf >/dev/null 2>/dev/null &
+
+  read -e -p "Enter filename without '.pdf' (Ctrl-C to delete scan): " NAME
+
+  mkdir -p "$PREFIX/$(dirname "$NAME")"
+  mkdir "$PREFIX/$NAME" # Should fail if the name already exists
+  mv -n page*.png "$PREFIX/$NAME/"
+  mv -n out.pdf "$PREFIX/$NAME.pdf"
+''
